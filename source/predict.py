efb72fa7a07fed251935340b8f147d5e3535b38f
DominikMartinat <darth@darth.localdomain>
2025-08-30 18:32:27 +0200

    alphamissense prediction rework

--- source/predict.py
+++ source/predict.py
@@ -4,17 +4,17 @@ import subprocess
 RES_DIR = Path.cwd().joinpath('results')
 DATA_DIR = Path.cwd().joinpath('data')
 
-def pymissense(uniprot_id, delete_pdf=True):
-    output_dir = RES_DIR.joinpath('pymissense')
-    output = output_dir.joinpath(uniprot_id+'-edit.pdb')
+def pymissense(am_file, pdb_file, uniprot_id, output_dir):
+    am_file = Path(am_file)
+    pdb_file = Path(pdb_file)
+    output_dir = Path(output_dir)
+    output = output_dir / f'{uniprot_id}-edit.pdb'
 
     #skip if prediction already exists
     if output.exists():
         print(f"AlphaMissense prediction for {uniprot_id} already exists")
         return
     
-    am_file = DATA_DIR.joinpath('AlphaMissense_aa_substitutions.tsv')
-    pdb_file = DATA_DIR.joinpath('pdb').joinpath(uniprot_id+'.pdb')
     command = f"pymissense --tsv {am_file} --pdbpath {pdb_file} {uniprot_id} {output_dir}"
     subprocess.check_output(command, shell=True, text=True)
     output_dir.joinpath(uniprot_id+'.pdf').unlink(missing_ok=True)
